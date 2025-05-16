9aebb1e88f2fba8923a689b2b1143db5be191cea
vanold101 <63750617+vanold101@users.noreply.github.com>
2025-05-16 10:06:29 -0400

    Fix: Change base path to relative for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/taska-loop/',
+  base: './',
   server: {
     port: 8080,
     host: true,
